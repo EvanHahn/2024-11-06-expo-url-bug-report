0a299a8328d8b7ef60f885d8f63b753f05d95570
Evan Hahn <me@evanhahn.com>
2024-11-06 17:25:10 +0000

    Test `URL.parse`

--- test.ts
+++ test.ts
@@ -1,5 +1,12 @@
 describe("URL", () => {
-  expect(new URL("https://例子.网站/").href).toBe(
-    "https://xn--fsqu00a.xn--5tzm5g/",
-  );
+  test("URL.parse", () => {
+    expect(URL.parse("")).toBeNull();
+    expect(URL.parse("https://example.com")?.host).toBe("example.com");
+  });
+
+  test("IDNs", () => {
+    expect(new URL("https://例子.网站/").href).toBe(
+      "https://xn--fsqu00a.xn--5tzm5g/",
+    );
+  });
 });
